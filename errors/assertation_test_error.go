975b60873c8ce080ba7466e5303250e95a43f0fa
Pietroski <AugustoPTSantos@gmail.com>
2021-04-08 21:57:04 -0300

    refactor: golang notification module refactored

--- errors/assertation_test_error.go
+++ errors/assertation_test_error.go
@@ -2,18 +2,17 @@ package errors
 
 import (
 	"fmt"
-	"github.com/Pietroski/notification/errors/models"
 	"log"
 )
 
 // AssertionTestError Handles a fatal error for assertion mistake
 // it receives as parameter the test number, the expected output and
 // the function returned result, respectively
-func (e *Errors) AssertionTestError(testNumber int, outputs models.VariadicParametersModel) {
-	o := outputs.Output
-	fro := outputs.FunctionReturnOutput
+func (e *Errors) AssertionTestError(testNumber int, expectedOutput interface{}, testedReturnOutput interface{}) {
+	o := expectedOutput
+	tro := testedReturnOutput
 
 	fmt.Printf("\nAssertation failed on test number %v!!\n", testNumber)
-	err := fmt.Errorf("\n Expected -> %v\n Returned -> %v\n", o, fro)
+	err := fmt.Errorf("\n Expected -> %v\n Returned -> %v\n", o, tro)
 	log.Fatalln(err)
 }
--- errors/errors.go
+++ errors/errors.go
@@ -7,6 +7,7 @@ var (
 type ErrorInterface interface {
 	CheckPanicError(err error, message ...string)
 	CheckFatalError(err error, message ...string)
+	AssertionTestError(testNumber int, expectedOutput interface{}, testedReturnOutput interface{})
 }
 
 type Errors struct {
